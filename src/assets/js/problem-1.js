1f94247a98741cbbea9f4345380ba510effef91c
Daniel Gonzalez <danielalexis.gp@gmail.com>
2024-03-05 09:05:10 -0700

    Added explanation of problem #1

--- src/assets/js/problem-1.js
+++ src/assets/js/problem-1.js
@@ -8,14 +8,16 @@ function handleReady (player) {
     player.on('loadedmetadata', handleLoadedMetadata);
 
     function handleLoadedMetadata () {
+        console.info('Video metadata has been loaded');
         player.muted(true);
+        console.info('Video player has been muted');
 
-
-        // Following the recommendations of the documentation I used player's setTimeout method
+        console.log('countdown started...');
         player.setTimeout(() => {
             const currentTime = player.currentTime();
             const newTime = currentTime + 20;
             player.currentTime(newTime);
+            console.log('Video has been skipped to', newTime);
         }, 10 * 1000);
     };
 };
